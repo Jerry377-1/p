6c64527357ca6057235d7f31cb9560a8fcbb7f84
Jerry377-1 <jerry377-1@gmail.com>
2025-03-10 10:30:40 +0800

    auto

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 git add -A
 git -c user.name=Jerry377-1 -c user.email=jerry377-1@gmail.com ci -mauto
-GIT_SSH_COMMAND=ssh -i /Users/ming/.ssh/jerry_rsa git push 
+GIT_SSH_COMMAND="ssh -i /Users/ming/.ssh/jerry_rsa" git push 
